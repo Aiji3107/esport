debc0422c4058bae2b260778d76459d3b8a2c543
Aiji3107 <aghikalam2@gmail.com>
2024-12-15 17:40:48 +0700

    `Fixed typos in NewsPage component`

--- app/news/page.js
+++ app/news/page.js
@@ -36,7 +36,7 @@ export default function NewsPage() {
               {/* Content */}
               <h3 className="text-balance text-justify text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
+                industry. Lorem Ipsum has been the industry&apos;ss standard dummy
                 text ever since the 1500s, when an unknown printer took a galley
                 of type and scrambled it to make a type specimen book. It has
                 survived not only five centuries, but also the leap into
@@ -78,7 +78,7 @@ export default function NewsPage() {
               {/* Content */}
               <h3 className="text-balance text-justify text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
+                industry. Lorem Ipsum has been the industry&apos;ss standard dummy
                 text ever since the 1500s, when an unknown printer took a galley
                 of type and scrambled it to make a type specimen book. It has
                 survived not only five centuries, but also the leap into
