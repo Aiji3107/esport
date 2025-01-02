e025b02df4c980a5fed0bb98c8ec503d533e67f9
Aghi Kalam <aghikalam2@gmail.com>
2025-01-02 14:19:19 +0700

    kk

--- app/news/page.js
+++ app/news/page.js
@@ -2,7 +2,8 @@ import Disclaimer from "@/components/disclaimer";
 import HeaderTitle from "@/components/HeaderTitle";
 import Image from "next/image";
 import Link from "next/link";
-import Photo1 from "@/components/assets/Group Photos/2023-champions-f0rsaken-something(1).jpg"
+import Photo1 from "@/components/assets/Group Photos/2023-champions-f0rsaken-something(1).jpg";
+import CardNews from "@/components/CardNews";
 
 export default function NewsPage() {
   return (
@@ -36,10 +37,10 @@ export default function NewsPage() {
               {/* Content */}
               <h3 className="text-balance text-justify text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry&apos;ss standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
+                industry. Lorem Ipsum has been the industry&apos;ss standard
+                dummy text ever since the 1500s, when an unknown printer took a
+                galley of type and scrambled it to make a type specimen book. It
+                has survived not only five centuries, but also the leap into
                 electronic typesetting, remaining essentially unchanged. It was
                 popularised in the 1960s with the release of Letraset sheets
                 containing Lorem Ipsum passages, and more recently with desktop
@@ -57,45 +58,42 @@ export default function NewsPage() {
         </div>
 
         {/* Card */}
-        <div className="flex flex-col justify-center items-center">
-          {/* Gambar */}
-          <Image
-            className="w-[340px] h-[190px] bg-white object-cover md:w-[720px] md:h-[406px] lg:w-[1920px] lg:h-[1085px]"
-            alt="gambar"
-            width={2048}
-            height={1366}
-            src={Photo1}
-          />
-          <div className="text-wrap">
-            {/* Date */}
-            <h1 className="text-left text-xs">Oct 11, 2024</h1>
-            <div className="text-pretty">
-              {/* title */}
-              <h1 className="text-center text-accent text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl font-bold my-2">
-                Paper Rex wins the 2024 SEA Games
-              </h1>
+        <CardNews
+          imageSrc={Photo1}
+          date="Oct 11, 2024"
+          title="Paper Rex wins the 2024 SEA Games"
+          content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+          link="/"
+        />
+        <div className="text-wrap">
+          {/* Date */}
+          <h1 className="text-left text-xs">Oct 11, 2024</h1>
+          <div className="text-pretty">
+            {/* title */}
+            <h1 className="text-center text-accent text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl font-bold my-2">
+              Paper Rex wins the 2024 SEA Games
+            </h1>
 
-              {/* Content */}
-              <h3 className="text-balance text-justify text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry&apos;ss standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.<span> </span>
-                {/* ReadMore */}
-                <Link className="text-red-600" href="/">
-                  ReadMore...
-                </Link>
-              </h3>
-            </div>
-
-            <div className="p-2" />
+            {/* Content */}
+            <h3 className="text-balance text-justify text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry&apos;ss standard dummy
+              text ever since the 1500s, when an unknown printer took a galley
+              of type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.<span> </span>
+              {/* ReadMore */}
+              <Link className="text-red-600" href="/">
+                ReadMore...
+              </Link>
+            </h3>
           </div>
+
+          <div className="p-2" />
         </div>
       </div>
     </div>
---
+++ components/CardNews.js
@@ -0,0 +1,10 @@
+
+import { motion } from "motion";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function CardNews({ imageSrc, date, title, content, link }) {
+  return (
+    <motion.div className="box" />
+  );
+}
